af0b39054500b109703d4dd9318263953e4e7c1b
openvela-robot <openvela-robot@xiaomi.com>
2018-10-11 10:24:49 +0800

    Add sys/types.h in utf8.h am: 18a1606a88
    Change-Id: I1be3c77db48c92f3e8d0b4a1b567e136c9aee5e6

--- logging.cpp
+++ logging.cpp
@@ -53,7 +53,6 @@
 #include <unistd.h>
 #endif
 
-#include <android-base/file.h>
 #include <android-base/macros.h>
 #include <android-base/parseint.h>
 #include <android-base/strings.h>
@@ -72,8 +71,14 @@ static const char* getprogname() {
   static char progname[MAX_PATH] = {};
 
   if (first) {
-    snprintf(progname, sizeof(progname), "%s",
-             android::base::Basename(android::base::GetExecutablePath()).c_str());
+    CHAR longname[MAX_PATH];
+    DWORD nchars = GetModuleFileNameA(nullptr, longname, arraysize(longname));
+    if ((nchars >= arraysize(longname)) || (nchars == 0)) {
+      // String truncation or some other error.
+      strcpy(progname, "<unknown>");
+    } else {
+      strcpy(progname, basename(longname));
+    }
     first = false;
   }
 
@@ -416,14 +421,6 @@ LogMessage::~LogMessage() {
     data_->GetBuffer() << ": " << strerror(data_->GetError());
   }
   std::string msg(data_->ToString());
-
-  if (data_->GetSeverity() == FATAL) {
-#ifdef __ANDROID__
-    // Set the bionic abort message early to avoid liblog doing it
-    // with the individual lines, so that we get the whole message.
-    android_set_abort_message(msg.c_str());
-#endif
-  }
 
   {
     // Do the actual logging with the lock held.

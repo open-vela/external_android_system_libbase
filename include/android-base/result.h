6558358a788ea7ce67db753630a340ed1a0544b7
liujinye <liujinye@xiaomi.com>
2019-06-06 23:27:29 +0800

    Merge "Move result.h from init to libbase"

--- include/android-base/result.h
+++ include/android-base/result.h
@@ -98,14 +98,6 @@ struct ResultError {
   int code_;
 };
 
-inline bool operator==(const ResultError& lhs, const ResultError& rhs) {
-  return lhs.message() == rhs.message() && lhs.code() == rhs.code();
-}
-
-inline bool operator!=(const ResultError& lhs, const ResultError& rhs) {
-  return !(lhs == rhs);
-}
-
 inline std::ostream& operator<<(std::ostream& os, const ResultError& t) {
   os << t.message();
   return os;

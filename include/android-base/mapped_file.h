676df7f18c8625a7af40dc888084f5848e259b42
liujinye <liujinye@xiaomi.com>
2019-08-14 14:28:40 +0800

    Merge "Fix more endian.h issues" am: 19b68a66e5
    Change-Id: I3b28b9ed60472c8dccac50eefd0a1a1c038714c1

--- include/android-base/mapped_file.h
+++ include/android-base/mapped_file.h
@@ -28,17 +28,15 @@
 #include <windows.h>
 #define PROT_READ 1
 #define PROT_WRITE 2
-using os_handle = HANDLE;
 #else
 #include <sys/mman.h>
-using os_handle = int;
 #endif
 
 namespace android {
 namespace base {
 
 /**
- * A region of a file mapped into memory (for grepping: also known as MmapFile or file mapping).
+ * A region of a file mapped into memory, also known as MmapFile.
  */
 class MappedFile {
  public:
@@ -51,32 +49,15 @@ class MappedFile {
                                             int prot);
 
   /**
-   * Same thing, but using the raw OS file handle instead of a CRT wrapper.
-   */
-  static MappedFile FromOsHandle(os_handle h, off64_t offset, size_t length, int prot);
-
-  /**
    * Removes the mapping.
    */
   ~MappedFile();
 
-  /**
-   * Not copyable but movable.
-   */
-  MappedFile(MappedFile&& other);
-  MappedFile& operator=(MappedFile&& other);
-
-  char* data() const { return base_ + offset_; }
-  size_t size() const { return size_; }
-
-  bool isValid() const { return base_ != nullptr; }
-
-  explicit operator bool() const { return isValid(); }
+  char* data() { return base_ + offset_; }
+  size_t size() { return size_; }
 
  private:
   DISALLOW_IMPLICIT_CONSTRUCTORS(MappedFile);
-
-  void Close();
 
   char* base_;
   size_t size_;

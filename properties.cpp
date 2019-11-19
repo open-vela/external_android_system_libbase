c976bc94e6b99bd1cedf7ad0cb58c3c5fc8ea5d0
liujinye <liujinye@xiaomi.com>
2019-11-19 15:41:37 +0800

    Merge "Add absl-like StringReplace." am: 43f9d5f34a
    Change-Id: Ia3b13412c1eeef0e67726f77f3b0d429c5d53ef9

--- properties.cpp
+++ properties.cpp
@@ -28,22 +28,19 @@
 #include <map>
 #include <string>
 
-#include <android-base/parsebool.h>
 #include <android-base/parseint.h>
 
 namespace android {
 namespace base {
 
 bool GetBoolProperty(const std::string& key, bool default_value) {
-  switch (ParseBool(GetProperty(key, ""))) {
-    case ParseBoolResult::kError:
-      return default_value;
-    case ParseBoolResult::kFalse:
-      return false;
-    case ParseBoolResult::kTrue:
-      return true;
+  std::string value = GetProperty(key, "");
+  if (value == "1" || value == "y" || value == "yes" || value == "on" || value == "true") {
+    return true;
+  } else if (value == "0" || value == "n" || value == "no" || value == "off" || value == "false") {
+    return false;
   }
-  __builtin_unreachable();
+  return default_value;
 }
 
 template <typename T>
